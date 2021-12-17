f7f31f1fad4b62bc41bf19edd44f68168f4c42b0
Rajappa <sarj@ukpc013845.home>
2021-12-17 16:20:51 +0000

    Changed application name

--- src/main/java/com/example/demo/TradeValidationApplication.java
+++ src/main/java/com/example/demo/TradeValidationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TradeValidationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TradeValidationApplication.class, args);
 	}
 
 }
